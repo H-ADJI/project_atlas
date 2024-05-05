314e0fb478ea1a24959f78cd8f6ed421e548e32f
h-adji <h-adji_tech@proton.me>
2024-05-05 11:29:23 +0200

    added test for illegale tokens

--- atlasPL/lexer/lexer_test.go
+++ atlasPL/lexer/lexer_test.go
@@ -40,4 +40,23 @@ func TestNextToken(t *testing.T) {
 			t.Errorf("Test failed [%d], should be : %v, but got %v ", i, tc, token)
 		}
 	}
+
+	sourceCode = "@ ;"
+	tests = []struct {
+		expectedType    TokenType
+		expectedLiteral string
+	}{
+		{ILLEGAL, "@"},
+		{SEMICOLON, ";"},
+		{EOF, ""},
+	}
+
+	lexer = NewLexer(readSource(sourceCode))
+	for i, tc := range tests {
+		token := lexer.NextToken()
+		fmt.Printf("type : %d ----- value : %s \n", token.Type, token.Literal)
+		if tc.expectedType != token.Type {
+			t.Errorf("Test failed [%d], should be : %v, but got %v ", i, tc, token)
+		}
+	}
 }
