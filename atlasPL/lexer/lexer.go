442ae016b96aa550d46a8890732c9a64638debbe
h-adji <h-adji_tech@proton.me>
2024-05-05 10:38:41 +0200

    first lexer version

--- atlasPL/lexer/lexer.go
+++ atlasPL/lexer/lexer.go
@@ -1,1 +1,119 @@
 package lexer
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"unicode"
+)
+
+type source struct {
+	filename string
+	reader   *bufio.Reader
+}
+
+func readSourceFile(filepath string) source {
+	// add closing the file somewhere XD
+	file, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println("could not open file : " + filepath)
+	}
+	r := bufio.NewReader(file)
+	return source{filename: filepath, reader: r}
+}
+func readSource(sourceCode string) source {
+	reader := bufio.NewReader(strings.NewReader(sourceCode))
+	return source{filename: "LocalStringVariable", reader: reader}
+}
+
+type Lexer struct {
+	currentLine int
+	currentRow  int
+	sourceCode  source
+}
+
+func NewLexer(s source) *Lexer {
+	l := new(Lexer)
+	l.sourceCode = s
+	return l
+}
+
+func (l *Lexer) NextToken() Token {
+	builder := new(strings.Builder)
+	for {
+		// read next character
+
+		char, _, err := l.sourceCode.reader.ReadRune()
+		if err == io.EOF {
+			return Token{Type: EOF}
+		}
+		if err != nil {
+			fmt.Println("an Error occured !!")
+		}
+		l.currentRow++
+
+		// handling new lines and white spaces
+		if char == '\n' {
+			l.currentLine++
+			l.currentRow = 0
+			continue
+		}
+		if char == ' ' {
+			l.currentRow++
+			continue
+		}
+
+		switch char {
+		case '+':
+			return Token{Type: PLUS, Literal: string(char)}
+		case '-':
+			return Token{Type: MINUS, Literal: string(char)}
+		case '/':
+			return Token{Type: DIV, Literal: string(char)}
+		case '*':
+			return Token{Type: MULT, Literal: string(char)}
+		case '(':
+			return Token{Type: LPAREN, Literal: string(char)}
+		case ')':
+			return Token{Type: RPAREN, Literal: string(char)}
+		case '}':
+			return Token{Type: RBRACE, Literal: string(char)}
+		case '{':
+			return Token{Type: LBRACE, Literal: string(char)}
+		case ',':
+			return Token{Type: COMMA, Literal: string(char)}
+		case ';':
+			return Token{Type: SEMICOLON, Literal: string(char)}
+		case '=':
+			return Token{Type: ASSIGN, Literal: string(char)}
+		}
+
+		// catching identifiers and keywords
+		for isLetter(char) {
+			builder.WriteRune(char)
+			char, _, err = l.sourceCode.reader.ReadRune()
+			if err == io.EOF {
+				return Token{Type: EOF}
+			}
+		}
+
+		// default case if nothing above works
+		return Token{Type: ILLEGAL}
+	}
+}
+
+func isLetter(char rune) bool {
+	return unicode.IsLetter(char) || char == '_'
+}
+
+func (l *Lexer) Peek() (rune, error) {
+	char, _, err := l.sourceCode.reader.ReadRune()
+	if err != nil {
+		return 0, err
+	}
+	l.sourceCode.reader.UnreadRune()
+
+	return char, nil
+}
--- atlasPL/lexer/lexer_test.go
+++ atlasPL/lexer/lexer_test.go
@@ -1,1 +1,30 @@
 package lexer
+
+import (
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	sourceCode := "=*+{},;"
+	tests := []struct {
+		expectedType    TokenType
+		expectedLiteral string
+	}{
+		{ASSIGN, "="},
+		{MULT, "*"},
+		{PLUS, "+"},
+		{LBRACE, "{"},
+		{RBRACE, "}"},
+		{COMMA, ","},
+		{SEMICOLON, ";"},
+		{EOF, ""},
+	}
+
+	lexer := NewLexer(readSource(sourceCode))
+	for i, tc := range tests {
+		token := lexer.NextToken()
+		if tc.expectedType != token.Type {
+			t.Errorf("Test failed [%d], should be : %v, but got %v ", i, tc, token)
+		}
+	}
+}
